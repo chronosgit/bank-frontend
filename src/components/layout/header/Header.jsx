0e6fa53e6e639f23707733e9519fdff441a00b23
chronosgit <warfacemeister@mail.ru>
2024-02-10 18:08:13 +0600

    Init a 'Header' layout component

---
+++ src/components/layout/header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./header.module.css";
+
+const Header = () => {
+
+    return (
+        <header className={styles.header}>
+            
+        </header>
+    )
+}
+
+export default Header;
