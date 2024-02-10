973ae3383153710805c36705544fdc4b17b61111
chronosgit <warfacemeister@mail.ru>
2024-02-10 18:10:35 +0600

    Added a 'NotFound' page component'

---
+++ src/pages/not-found/NotFound.jsx
@@ -0,0 +1,12 @@
+import styles from "./not-found.module.css";
+
+const NotFound = () => {
+
+    return (
+        <div className={styles.page}>
+            
+        </div>
+    )
+}
+
+export default NotFound;
